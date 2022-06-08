c250e1a2c5c52c2a139ba40b4f11b8d3f0bd3448
mnbarber <mnbarber1993@gmail.com>
2022-06-08 13:13:45 -0700

    fixing controllers

--- controllers/books.js
+++ controllers/books.js
@@ -1,24 +1,15 @@
 const express = require('express');
-const { Books } = require('../models');
 const router = express.Router();
+const db = require('../models');
 
-router.get('/search', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
-        res.json(
-            await Books.find({
-                name: { $regex: req.query.query, $options: "i" },
-            }).limit(20)
-        );
+        const books = await db.Book.find({});
+        const context = { books };
+        return res.render(context);
     } catch(error) {
-        res.status(400).json(error);
-    }
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-        res.json(await Books.findById(req.params.id));
-    } catch(error) {
-        res.status(400).json(error);
+        req.error = error;
+        return next();
     }
 });
 
