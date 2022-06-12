a32035e04e930f0839e45daf237c7f0f2f0cae5f
mnbarber <mnbarber1993@gmail.com>
2022-06-12 13:18:11 -0700

    adjusting cors

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors(), function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/books', controllers.books);
